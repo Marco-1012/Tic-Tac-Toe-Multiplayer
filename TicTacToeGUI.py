b37a467f7914ae4881780fc35190a22ef2cea9d3
Daniel1353 <dpleyer@tsn.at>
2024-06-06 10:44:46 +0200

    When button is pressed it gets destroyed and replaced with text

--- TicTacToeGUI.py
+++ TicTacToeGUI.py
@@ -1,5 +1,11 @@
 from tkinter import *
 
+def button_press(row, col):
+
+    buttons[row][col].grid_forget()
+    ersetzen = Label(fenster, text = "x")
+    ersetzen.grid(row = row, column = col)
+
 buttons=[]
 
 fenster = Tk()
@@ -10,7 +16,7 @@ for r in range(3):
     row=[]
 
     for c in range(3):
-        b = Button(text = "Hallo", height = 9, width = 19)
+        b = Button(text = "Hallo", height = 9, width = 19, command = lambda r = r, c = c: button_press(r, c))
         b.grid(row = r, column = c)
         row.append(b)
     buttons.append(row)
