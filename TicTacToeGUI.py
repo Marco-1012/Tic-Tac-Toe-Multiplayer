548d04832983c9b38df8658e7e56aa1a77b55b55
Daniel1353 <dpleyer@tsn.at>
2024-06-06 10:36:45 +0200

    Field now look a little better :)

--- TicTacToeGUI.py
+++ TicTacToeGUI.py
@@ -4,17 +4,18 @@ buttons=[]
 
 fenster = Tk()
 
-fenster.geometry("400x400")
+fenster.geometry("420x430")
 
 for r in range(3):
     row=[]
 
     for c in range(3):
-        b = Button(text = "Hallo")
+        b = Button(text = "Hallo", height = 9, width = 19)
         b.grid(row = r, column = c)
         row.append(b)
     buttons.append(row)
     
 print(buttons)
 buttons[0][1].config(text = "Tshuss")
+
 fenster.mainloop()
