fcc0b31eb620354f311296a9408f583a4c1610ce
Daniel1353 <dpleyer@tsn.at>
2024-06-19 11:49:17 +0200

    small things

--- TicTacToeGUI.py
+++ TicTacToeGUI.py
@@ -1,21 +1,22 @@
 from tkinter import *
 
-def button_press(row, col, counter):
-    if counter[0] % 2 == 0:
+def button_press(row, col):
+    global counter
+    if counter % 2 == 0:
         buttons[row][col].config(text="X", state=DISABLED)
     else:
         buttons[row][col].config(text="O", state=DISABLED)
 
     pressedButtonrow = row
     pressedButtoncol = col
-    counter[0] += 1
-    print(pressedButtonrow, pressedButtoncol, counter[0])
+    counter += 1
+    print(pressedButtonrow, pressedButtoncol, counter)
 
 
 buttons = []
 pressedButtonrow = 0
 pressedButtoncol = 0
-counter = [0] 
+counter = 0
 
 fenster = Tk()
 fenster.geometry("420x430")
@@ -24,7 +25,7 @@ for r in range(3):
     row = []
 
     for c in range(3):
-        b = Button(text="", height=9, width=19, command=lambda r=r, c=c: button_press(r, c, counter))
+        b = Button(text="", height=9, width=19, command=lambda r=r, c=c: button_press(r, c))
         b.grid(row=r, column=c)
         row.append(b)
     buttons.append(row)
